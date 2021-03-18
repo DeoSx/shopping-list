432701158201a589843779c27288da363644993f
DeoSx <Deosxz123@gmail.com>
2021-03-18 22:53:53 +0600

    feat(components): infobar

---
+++ src/components/InfoBar/index.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+import { InfoBarProps } from '../../types/components/InfoBar'
+
+const InfoBar: React.FC<InfoBarProps> = (props) => {
+  return (
+    <div className="infobar">
+      <Link to="/">back</Link>
+      <img src={props.image} alt="" />
+      <div className="infobar__item">
+        <span>name</span>
+        <p>{props.name}</p>
+      </div>
+      <div className="infobar__item">
+        <span>note</span>
+        <p>{props.note}</p>
+      </div>
+    </div>
+  )
+}
+
+export default InfoBar
